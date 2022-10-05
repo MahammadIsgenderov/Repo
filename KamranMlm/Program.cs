94a3f86b8fd134dcc99312fddcbb0dd726533360
MahammadIsgenderov <isgenderovmahammad4@gmail.com>
2022-10-05 16:51:01 +0400

    Init

---
+++ KamranMlm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KamranMlm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Kamran MLM");
+        }
+    }
+}
